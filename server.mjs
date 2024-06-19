b342c0b06d503f991aa6d8d425f02d369cc53fab
Ruth Boateng <ruthboatengs1@gmail.com>
2024-06-19 09:29:59 +0000

    learn node,js

---
+++ server.mjs
@@ -0,0 +1,15 @@
+import { createServer } from "node:http"; 
+
+const server = createServer((req, res) => {
+    res.writeHead(500, {'Content-Type': 'text/html'});
+    res.end('Server is running');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server is listening');
+});
+// the 127...is the host, and 3000 is a port used to define the address.
+
+// Arrow functions
+function handleRequest() {}
+const handleReguest = () => {}
